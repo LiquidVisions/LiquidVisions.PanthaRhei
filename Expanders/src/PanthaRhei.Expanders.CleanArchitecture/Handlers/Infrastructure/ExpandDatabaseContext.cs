c0eced0cbcad3bf4511731019a1a5a258b8da670
Gerco Koks <gerco.koks@outlook.com>
2023-01-22 15:00:06 +0100

    fixed ExpandDatabaseContext

--- Expanders/src/PanthaRhei.Expanders.CleanArchitecture/Handlers/Infrastructure/ExpandDatabaseContext.cs
+++ Expanders/src/PanthaRhei.Expanders.CleanArchitecture/Handlers/Infrastructure/ExpandDatabaseContext.cs
@@ -1,7 +1,8 @@
 ﻿using System.Linq;
+using LiquidVisions.PanthaRhei.Generator.Domain;
 using LiquidVisions.PanthaRhei.Generator.Domain.Entities;
 using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Dependencies;
-using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Generators.Handlers;
+using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Generators;
 using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Templates;
 
 namespace LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastructure
@@ -9,10 +10,15 @@ namespace LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastr
     /// <summary>
     /// Generates the DbContext class into the Infrastructure library.
     /// </summary>
-    public class ExpandDatabaseContext : AbstractHandlerInteractor<CleanArchitectureExpander>
+    public class ExpandDatabaseContext : IHandlerInteractor<CleanArchitectureExpander>
     {
         private readonly IProjectAgentInteractor projectAgent;
         private readonly ITemplateInteractor templateService;
+        private readonly Parameters parameters;
+        private readonly App app;
+        private readonly Component domain;
+        private readonly CleanArchitectureExpander expander;
+        private readonly Component infrastructure;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ExpandDatabaseContext"/> class.
@@ -20,33 +26,42 @@ namespace LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastr
         /// <param name="expander"><seealso cref="CleanArchitectureExpander"/></param>
         /// <param name="dependencyFactory"><seealso cref="IDependencyFactoryInteractor"/></param>
         public ExpandDatabaseContext(CleanArchitectureExpander expander, IDependencyFactoryInteractor dependencyFactory)
-            : base(expander, dependencyFactory)
         {
+            this.expander = expander;
+
             projectAgent = dependencyFactory.Get<IProjectAgentInteractor>();
             templateService = dependencyFactory.Get<ITemplateInteractor>();
+            parameters = dependencyFactory.Get<Parameters>();
+            app = dependencyFactory.Get<App>();
+
+            domain = Expander.Model.GetComponentByName(Resources.Domain);
+            infrastructure = Expander.Model.GetComponentByName(Resources.EntityFramework);
         }
 
-        public override int Order => 9;
+        public int Order => 9;
+
+        public string Name => nameof(ExpandDatabaseContext);
+
+        public CleanArchitectureExpander Expander => expander;
+
+        public bool CanExecute => parameters.CanExecuteDefaultAndExtend();
 
         /// <inheritdoc/>
-        public override void Execute()
+        public void Execute()
         {
-            Component domain = Expander.Model.GetComponentByName(Resources.Domain);
-            Component infrastructure = Expander.Model.GetComponentByName(Resources.EntityFramework);
-
-            var parameters = new
+            var templateModel = new
             {
-                App.Entities,
-                ConnectionString = App.ConnectionStrings.Single(x => x.Name == "DefaultConnectionString").Definition,
-                NameSpace = infrastructure.GetComponentNamespace(App),
-                NameSpaceEntities = domain.GetComponentNamespace(App, Resources.DomainEntityFolder),
+                app.Entities,
+                //ConnectionString = app.ConnectionStrings.Single(x => x.Name == "DefaultConnectionString").Definition,
+                //NameSpace = infrastructure.GetComponentNamespace(app),
+                //NameSpaceEntities = domain.GetComponentNamespace(app, Resources.DomainEntityFolder),
             };
 
-            string fullPathToTemplate = Expander.Model.GetTemplateFolder(Parameters, Resources.DbContextTemplate);
-            string result = templateService.Render(fullPathToTemplate, parameters);
+            string fullPathToTemplate = Expander.Model.GetTemplateFolder(parameters, Resources.DbContextTemplate);
+            string fullPathToComponent = projectAgent.GetComponentOutputFolder(infrastructure);
+            string path = System.IO.Path.Combine(fullPathToComponent, "Context.cs");
 
-            string path = System.IO.Path.Combine(projectAgent.GetComponentOutputFolder(infrastructure), "Context.cs");
-            File.WriteAllText(path, result);
+            templateService.RenderAndSave(fullPathToTemplate, templateModel, path);
         }
     }
 }
