eebf54ac2e29214f5e2a0121e4ea31a5e7e86784
Gerco Koks <gerco.koks@outlook.com>
2023-02-04 14:58:01 +0100

    added ExpandRepositoryHandlerInteractorTests

--- Expanders/src/PanthaRhei.Expanders.CleanArchitecture/Handlers/Infrastructure/ExpandRepositoryHandlerInteractor.cs
+++ Expanders/src/PanthaRhei.Expanders.CleanArchitecture/Handlers/Infrastructure/ExpandRepositoryHandlerInteractor.cs
@@ -1,17 +1,28 @@
-﻿using LiquidVisions.PanthaRhei.Generator.Domain.Entities;
+﻿using System.ComponentModel;
+using System.IO;
+using LiquidVisions.PanthaRhei.Generator.Domain;
+using LiquidVisions.PanthaRhei.Generator.Domain.Entities;
 using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Dependencies;
+using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Generators;
 using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Generators.Handlers;
 using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Templates;
+using LiquidVisions.PanthaRhei.Generator.Domain.IO;
 
 namespace LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastructure
 {
     /// <summary>
     /// Generates the DbContext class into the Infrastructure library.
     /// </summary>
-    public class ExpandRepositoryHandlerInteractor : AbstractHandlerInteractor<CleanArchitectureExpander>
+    public class ExpandRepositoryHandlerInteractor : IExpanderHandlerInteractor<CleanArchitectureExpander>
     {
         private readonly IProjectAgentInteractor projectAgent;
         private readonly ITemplateInteractor templateService;
+        private readonly Parameters parameters;
+        private readonly App app;
+        private readonly Generator.Domain.Entities.Component component;
+        private readonly Generator.Domain.Entities.Component applicationComponent;
+        private readonly CleanArchitectureExpander expander;
+        private readonly string fullPathToTemplate;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ExpandRepositoryHandlerInteractor"/> class.
@@ -19,36 +30,46 @@ namespace LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastr
         /// <param name="expander"><seealso cref="CleanArchitectureExpander"/></param>
         /// <param name="dependencyFactory"><seealso cref="IDependencyFactoryInteractor"/></param>
         public ExpandRepositoryHandlerInteractor(CleanArchitectureExpander expander, IDependencyFactoryInteractor dependencyFactory)
-            : base(expander, dependencyFactory)
         {
+            this.expander = expander;
+
             projectAgent = dependencyFactory.Get<IProjectAgentInteractor>();
             templateService = dependencyFactory.Get<ITemplateInteractor>();
+            parameters = dependencyFactory.Get<Parameters>();
+            app = dependencyFactory.Get<App>();
+
+            component = expander.Model.GetComponentByName(Resources.EntityFramework);
+            applicationComponent = expander.Model.GetComponentByName(Resources.Application);
+
+            string path = System.IO.Path.Combine(projectAgent.GetComponentOutputFolder(component), Resources.RepositoryFolder);
+            dependencyFactory.Get<IDirectory>().Create(path);
+
+            fullPathToTemplate = Expander.Model.GetTemplateFolder(parameters, Resources.RepositoryTemplate);
         }
 
-        public override int Order => 11;
+        public int Order => 11;
+
+        public string Name => nameof(ExpandRepositoryHandlerInteractor);
+
+        public CleanArchitectureExpander Expander => expander;
+
+        public bool CanExecute => parameters.CanExecuteDefaultAndExtend();
 
         /// <inheritdoc/>
-        public override void Execute()
+        public void Execute()
         {
-            Component component = Expander.Model.GetComponentByName(Resources.EntityFramework);
-            Component applicationComponent = Expander.Model.GetComponentByName(Resources.Application);
-            string path = System.IO.Path.Combine(projectAgent.GetComponentOutputFolder(component), Resources.RepositoryFolder);
-            Directory.Create(path);
-
-            foreach (Entity entity in App.Entities)
+            foreach (Entity entity in app.Entities)
             {
-                var parameters = new
+                var templateModel = new
                 {
                     entity,
                     component,
                     applicationComponent,
                 };
 
-                string fullPathToTemplate = Expander.Model.GetTemplateFolder(Parameters, Resources.RepositoryTemplate);
-                string result = templateService.Render(fullPathToTemplate, parameters);
+                string filePath = Path.Combine(projectAgent.GetComponentOutputFolder(component), $"{entity.Name}Repository.cs");
+                templateService.RenderAndSave(fullPathToTemplate, templateModel, filePath);
 
-                string filePath = System.IO.Path.Combine(projectAgent.GetComponentOutputFolder(component), $"{entity.Name}Repository.cs");
-                File.WriteAllText(filePath, result);
             }
         }
     }
---
+++ Expanders/tests/PanthaRhei.Expanders.CleanArchitecture.Tests/Handlers/Infrastructure/ExpandRepositoryHandlerInteractorTests.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LiquidVisions.PanthaRhei.Expanders.CleanArchitecture;
+using LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Handlers.Infrastructure;
+using LiquidVisions.PanthaRhei.Generator.Domain;
+using LiquidVisions.PanthaRhei.Generator.Domain.Entities;
+using LiquidVisions.PanthaRhei.Generator.Domain.Interactors.Templates;
+using Moq;
+using Xunit;
+using CleanArchitectureResources = LiquidVisions.PanthaRhei.Expanders.CleanArchitecture.Resources;
+
+namespace LiquidVisions.PanthaRhei.Generator.CleanArchitecture.Tests.Handlers.Infrastructure
+{
+    public class ExpandRepositoryHandlerInteractorTests
+    {
+        private readonly ExpandRepositoryHandlerInteractor handler;
+        private readonly string outputFolder;
+        private readonly CleanArchitectureFakes fakes = new();
+        private readonly List<Entity> allEntities;
+        private readonly App app;
+
+        public ExpandRepositoryHandlerInteractorTests()
+        {
+            allEntities = CleanArchitectureFakes.GetValidEntities();
+            app = fakes.SetupApp(allEntities);
+            fakes.IProjectAgentInteractor.Setup(x => x.GetComponentOutputFolder(fakes.InfrastructureComponent.Object)).Returns(fakes.ExpectedCompontentOutputFolder);
+            fakes.MockCleanArchitectureExpander(allEntities);
+            handler = new(fakes.CleanArchitectureExpander.Object, fakes.IDependencyFactoryInteractor.Object);
+
+            outputFolder = Path.Combine(fakes.IProjectAgentInteractor.Object.GetComponentOutputFolder(fakes.InfrastructureComponent.Object), CleanArchitectureResources.RepositoryFolder);
+        }
+
+        [Fact]
+        public void Constructor_ShouldValidate()
+        {
+            // arrange
+            // act
+            // assert
+            fakes.IDependencyFactoryInteractor.Verify(x => x.Get<IProjectAgentInteractor>(), Times.Once);
+            fakes.IDependencyFactoryInteractor.Verify(x => x.Get<ITemplateInteractor>(), Times.Once);
+            fakes.IDependencyFactoryInteractor.Verify(x => x.Get<Parameters>(), Times.Once);
+            fakes.IDependencyFactoryInteractor.Verify(x => x.Get<App>(), Times.Once);
+            fakes.IDependencyFactoryInteractor.Verify(x => x.Get<It.IsAnyType>(), Times.Exactly(5));
+        }
+
+        [Fact]
+        public void Constructor_ShouldCreateOutputDirectory()
+        {
+            // arrange
+            // act
+            // assert
+            fakes.IDirectory.Verify(x => x.Create(It.IsAny<string>()), Times.Once);
+            fakes.IDirectory.Verify(x => x.Create(outputFolder), Times.Once);
+        }
+
+
+        [Fact]
+        public void Order_ShouldValidate()
+        {
+            // arrange
+            // act
+            // assert
+            Assert.Equal(11, handler.Order);
+        }
+
+        [Fact]
+        public void Name_ShouldBeEqual()
+        {
+            // arrange
+            // act
+            // assert
+            Assert.Equal(nameof(ExpandRepositoryHandlerInteractor), handler.Name);
+        }
+
+        [Theory]
+        [InlineData(GenerationModes.Default, true)]
+        [InlineData(GenerationModes.Migrate, false)]
+        [InlineData(GenerationModes.Extend, true)]
+        [InlineData(GenerationModes.Deploy, false)]
+        [InlineData(GenerationModes.None, false)]
+        public void CanExecute_ShouldBeFalse(GenerationModes mode, bool expectedResult)
+        {
+            // arrange
+            fakes.Parameters.Setup(x => x.GenerationMode).Returns(mode);
+
+            // act
+            // assert
+            Assert.Equal(expectedResult, handler.CanExecute);
+        }
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, true)]
+        public void CanExecute_ShouldOnlyBeTrueWhenCleanParameterIsSetToTrue(bool clean, bool expectedResult)
+        {
+            // arrange
+            fakes.Parameters.Setup(x => x.GenerationMode).Returns(GenerationModes.Default);
+            fakes.Parameters.Setup(x => x.Clean).Returns(clean);
+
+            // act
+            // assert
+            Assert.Equal(expectedResult, handler.CanExecute);
+        }
+
+        [Fact]
+        public void Execute_ShouldCreateAndSaveTemplate()
+        {
+            // arrange
+            string expectedTemplateBaseBath = Path.Combine(fakes.Parameters.Object.ExpandersFolder, fakes.CleanArchitectureExpander.Object.Model.Name, fakes.CleanArchitectureExpander.Object.Model.TemplateFolder, $"{CleanArchitectureResources.RepositoryTemplate}.template");
+
+
+            // act
+            handler.Execute();
+
+            // assert
+            fakes.ITemplateInteractor.Verify(x => x.RenderAndSave(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<string>()), Times.Exactly(allEntities.Count));
+
+            foreach (Entity entity in allEntities)
+            {
+                string fullSavePath = Path.Combine(
+                    fakes.IProjectAgentInteractor.Object.GetComponentOutputFolder(fakes.InfrastructureComponent.Object),
+                    $"{entity.Name}Repository.cs");
+
+                fakes.ITemplateInteractor.Verify(
+                    x => x.RenderAndSave(
+                        expectedTemplateBaseBath,
+                        It.Is<object>(x =>
+                        x.GetHashCode() == new
+                        {
+                            entity,
+                            component = fakes.InfrastructureComponent.Object,
+                            applicationComponent = fakes.ApplicationComponent.Object,
+                        }.GetHashCode()),
+                        fullSavePath),
+                    Times.Once);
+            }
+        }
+    }
+}
